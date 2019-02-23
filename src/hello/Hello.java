5567fe88f6dc4f5aef70e512cfee2de9c70c8349
PallaviGupta21 <guptapallavi200@gmail.com>
2019-02-23 15:12:32 +0530

    first

---
+++ src/hello/Hello.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Hello {
+String a[];
+
+
+public String[] getA() {
+	return a;
+}
+
+
+public void setA(String[] a) {
+	this.a = a;
+}
+
+}
---
+++ src/hello/Welcome.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("enter string : ");
+     Scanner sc=new Scanner(System.in);
+     String a=sc.next();
+     System.out.println("your string is : "+a);
+     sc.close();
+     
+	}
+ 
+ 
+}
